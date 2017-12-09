bc63d244f2a8e3ed85d970ad94c54459dffafe21
Kristian Kirov <kristiankirov9112@gmail.com>
2017-12-10 00:38:58 +0200

    Fixed: bulgarian prices operations not working while creating variable product.

--- wp-content/themes/maison/assets/js/admin.js
+++ wp-content/themes/maison/assets/js/admin.js
@@ -1,4 +1,5 @@
 jQuery(function($) {
+    var initialized = false;
     var customVariableActions = [
         'wcpbc_variable_regular_price',
         'wcpbc_variable_regular_price_increase',
@@ -8,38 +9,48 @@ jQuery(function($) {
         'wcpbc_variable_sale_price_decrease'
     ];
 
-    var ajaxDataEventSuffix = '_ajax_data';
-    var customVariableActionEvents = customVariableActions.map(function(a) { return a + ajaxDataEventSuffix; }).join(' ');
-    $('select.variation_actions').on(customVariableActionEvents,
-    function(e, data){
-        var customVariableAction = e.type.substring(0, e.type.length - ajaxDataEventSuffix.length)
-        switch (customVariableAction) {
-            case 'wcpbc_variable_regular_price_increase':
-            case 'wcpbc_variable_regular_price_decrease':
-            case 'wcpbc_variable_sale_price_increase':
-            case 'wcpbc_variable_sale_price_decrease':
-                value = window.prompt(woocommerce_admin_meta_boxes_variations.i18n_enter_a_value_fixed_or_percent);
-                if ( value != null ) {
-                    if ( value.indexOf( '%' ) >= 0 ) {
-                        data.value = accounting.unformat( value.replace( /\%/, '' ), woocommerce_admin.mon_decimal_point ) + '%';
-                    } else {
-                        data.value = accounting.unformat( value, woocommerce_admin.mon_decimal_point );
-                    }
-                }
-
-                break;
-            case 'wcpbc_variable_regular_price':
-            case 'wcpbc_variable_sale_price':
-                value = window.prompt(woocommerce_admin_meta_boxes_variations.i18n_enter_a_value);
-                if ( value != null ) {
-                    data.value = value;
-                }
-
-                break;
+    $('.wc-metaboxes-wrapper').on(customVariableActions.join(' '), 'select.variation_actions', init);
+    
+    function init() {
+        if (initialized) {
+            return;
         }
 
-        data['zone_id'] = $(this.options[this.selectedIndex]).data('zoneId');
+        var ajaxDataEventSuffix = '_ajax_data';
+        var customVariableActionEvents = customVariableActions.map(function(a) { return a + ajaxDataEventSuffix; }).join(' ');
+        $('select.variation_actions').on(customVariableActionEvents,
+        function(e, data) {
+            var customVariableAction = e.type.substring(0, e.type.length - ajaxDataEventSuffix.length)
+            switch (customVariableAction) {
+                case 'wcpbc_variable_regular_price_increase':
+                case 'wcpbc_variable_regular_price_decrease':
+                case 'wcpbc_variable_sale_price_increase':
+                case 'wcpbc_variable_sale_price_decrease':
+                    value = window.prompt(woocommerce_admin_meta_boxes_variations.i18n_enter_a_value_fixed_or_percent);
+                    if ( value != null ) {
+                        if ( value.indexOf( '%' ) >= 0 ) {
+                            data.value = accounting.unformat( value.replace( /\%/, '' ), woocommerce_admin.mon_decimal_point ) + '%';
+                        } else {
+                            data.value = accounting.unformat( value, woocommerce_admin.mon_decimal_point );
+                        }
+                    }
 
-        return data;
-    });
+                    break;
+                case 'wcpbc_variable_regular_price':
+                case 'wcpbc_variable_sale_price':
+                    value = window.prompt(woocommerce_admin_meta_boxes_variations.i18n_enter_a_value);
+                    if ( value != null ) {
+                        data.value = value;
+                    }
+
+                    break;
+            }
+
+            data['zone_id'] = $(this.options[this.selectedIndex]).data('zoneId');
+
+            return data;
+        });
+
+        initialized = true;
+    }
 });
