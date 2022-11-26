e983b5e5ec0298d3d188db500e9782b147341519
Kristian Kirov <kristian.kirov@icanpreneur.com>
2022-11-26 17:33:54 +0200

    Update gulp to work with lates version of npm and node

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,44 @@ const maisonThemeBaseDir = 'wp-content/themes/maison';
 const jsFilesToMinify = [
     path.join(maisonThemeBaseDir, 'assets/js/global.js'),
     path.join(maisonThemeBaseDir, 'assets/js/checkout.js'),
-    path.join(maisonThemeBaseDir, 'assets/js/admin.js')
+    path.join(maisonThemeBaseDir, 'assets/js/admin.js'),
 ];
 
 const cssFilesToMinify = [
     path.join(maisonThemeBaseDir, 'assets/css/bootstrap.css'),
     path.join(maisonThemeBaseDir, 'assets/css/fontello.css'),
     path.join(maisonThemeBaseDir, 'assets/css/checkout.css'),
-    path.join(maisonThemeBaseDir, 'style.css')
+    path.join(maisonThemeBaseDir, 'style.css'),
 ];
 
-gulp.task('minify-js', () => { 
-    gulp.src(jsFilesToMinify)
+gulp.task('minify-js', () => {
+    return gulp
+        .src(jsFilesToMinify)
         .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest(f => f.base))
+        .pipe(
+            rename({
+                suffix: '.min',
+            })
+        )
+        .pipe(gulp.dest(f => f.base));
 });
 
-gulp.task('minify-css', () => { 
-    gulp.src(cssFilesToMinify)
+gulp.task('minify-css', () => {
+    return gulp
+        .src(cssFilesToMinify)
         .pipe(cleanCss({ rebase: false }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest(f => f.base))
+        .pipe(
+            rename({
+                suffix: '.min',
+            })
+        )
+        .pipe(gulp.dest(f => f.base));
 });
 
-gulp.task('build', ['minify-js', 'minify-css']);
+gulp.task('build', gulp.series('minify-js', 'minify-css'));
 
 gulp.task('watch', () => {
-    gulp.watch(jsFilesToMinify.concat(cssFilesToMinify), ['build']);
+    return gulp.watch(jsFilesToMinify.concat(cssFilesToMinify), gulp.series('build'));
 });
 
-gulp.task('default', ['build'])
+gulp.task('default', gulp.series('build'));
